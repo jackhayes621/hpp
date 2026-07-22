d46db690599130661cc4544147881c3c3eb84cd2
jackhayes621 <jackhayes621@gmail.com>
2026-07-22 19:41:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "hp-printer.html"));
 });
 
+app.get("*", (req, res) => {
+    const file = path.join(__dirname, req.path);
+
+    res.sendFile(file, err => {
+        if (err) {
+            res.sendFile(path.join(__dirname, "hp-printer.html"));
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
